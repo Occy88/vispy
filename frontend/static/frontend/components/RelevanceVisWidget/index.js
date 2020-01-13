da2760dd1a5023588e0fc6e7c459bb71d74409b5
Roger Milroy <rogermilroy@icloud.com>
2020-01-13 20:31:04 +0000

    Refactor nivo imports.

--- frontend/static/frontend/components/RelevanceVisWidget/index.js
+++ frontend/static/frontend/components/RelevanceVisWidget/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import BaseWidget from "../../../../../static/components/BaseWidget";
-import ResponsiveHeatMap from "nivo/lib/components/charts/heatmap/ResponsiveHeatMap";
+import {ResponsiveHeatMap} from "@nivo/heatmap"
 import WidgetHeader from "../WidgetHeader";
 import WidgetBody from "../WidgetBody";
+import "./style.scss"
 
 /**
  * An Example of a simple widget to be used in the grid.
@@ -19,10 +20,19 @@ const RelevanceVisWidget = (handleRemove, i) => {
 
   // these would be fetched.
   const data = [
-      {row:0, '0': 0.3, '1': 0.1, '2': 0.4, '3': 0.1},
-      {row:1, '0': 0.7, '1': 0.1, '2': 0.6, '3': 0.1},
-      {row:2, '0': 0.2, '1': 0.01, '2': 0.4, '3': 0.1},
-      {row:3, '0': 0.9, '1': 0.1, '2': 0.4, '3': 0.1}
+      {row:'0', '0': 0.3, '1': 0.1, '2': 0.4, '3': 0.2, '4': 0.3, '5': 0.1, '6': 0.3, '7': 0.8},
+      {row:'1', '0': 0.7, '1': 0.1, '2': 0.6, '3': 0.8, '4': 0.5, '5': 0.1, '6': 0.6, '7': 0.1},
+      {row:'2', '0': 0.2, '1': 0.0, '2': 0.4, '3': 0.9, '4': 0.7, '5': 0.1, '6': 0.2, '7': 0.5},
+      {row:'3', '0': 0.3, '1': 0.1, '2': 0.5, '3': 0.3, '4': 0.6, '5': 0.1, '6': 0.4, '7': 0.1},
+      {row:'4', '0': 0.6, '1': 0.3, '2': 0.7, '3': 0.5, '4': 0.7, '5': 0.2, '6': 0.6, '7': 0.5},
+      {row:'5', '0': 0.9, '1': 0.3, '2': 0.3, '3': 0.7, '4': 0.9, '5': 0.4, '6': 0.7, '7': 0.3},
+      {row:'6', '0': 0.6, '1': 0.7, '2': 0.3, '3': 0.1, '4': 0.2, '5': 0.2, '6': 0.6, '7': 0.1},
+      {row:'7', '0': 0.7, '1': 0.3, '2': 0.4, '3': 0.4, '4': 0.6, '5': 0.4, '6': 0.1, '7': 0.3},
+      {row:'8', '0': 0.3, '1': 0.1, '2': 0.3, '3': 0.1, '4': 0.1, '5': 0.2, '6': 0.6, '7': 0.2}
+  ];
+
+  const alt_data = [
+      {row:0, dat: [ 0.3, 0.1, 0.4, 0.2, 0.34, 0.1, 0.37, 0.08]}
   ];
 
   // Create the content using the BaseWidget component.
@@ -35,10 +45,14 @@ const RelevanceVisWidget = (handleRemove, i) => {
       <WidgetBody>
         <ResponsiveHeatMap
             data={data}
-            keys={['0', '1', '2', '3']}
+            keys={['0', '1', '2', '3', '4', '5', '6', '7']}
+            margin={{ top: 10, right: 10, bottom: 10, left: 10 }}
             indexBy="row"
             forceSquare={true}
-            colors="RdBu"
+            colors="reds"
+            axisLeft={null}
+            axisTop={null}
+            enableLabels={false}
         />
       </WidgetBody>
     </BaseWidget>
