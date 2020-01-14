c95cd872acd7919132dc05dffe6e81ebfce113a8
Roger Milroy <rogermilroy@icloud.com>
2020-01-14 14:59:02 +0000

    Add some styling/formatting to DeepLearningVisWidget.

--- frontend/static/frontend/components/DeepLearningVisWidget/index.js
+++ frontend/static/frontend/components/DeepLearningVisWidget/index.js
@@ -14,7 +14,7 @@ import "./style.scss"
  *  - Create a button for it in the DashboardToolbar
  *
  * Any given widget must take the following parameters
- * @param {function} handleRemove callback used to remove this widget
+ * @param {function} handleRemove callback usSSSSSed to remove this widget
  * @param {string} i unique identity of this widget.
  */
 const DeepLearningVisWidget = (handleRemove, i) => {
@@ -32,72 +32,84 @@ const DeepLearningVisWidget = (handleRemove, i) => {
       {row:'8', '0': 0.3, '1': 0.1, '2': 0.3, '3': 0.1, '4': 0.1, '5': 0.2, '6': 0.6, '7': 0.2}
   ];
 
-  const timeline_posneg_data = [
-      {minibatch:0, pos:0.1, neg:0.0},
-      {minibatch:0, pos:0.3, neg:0.1},
-      {minibatch:0, pos:0.4, neg:0.2},
-      {minibatch:0, pos:0.7, neg:0.1},
-      {minibatch:0, pos:0.2, neg:0.3},
-      {minibatch:0, pos:0.8, neg:0.4},
-      {minibatch:0, pos:0.7, neg:0.5},
-      {minibatch:0, pos:0.4, neg:0.4},
-      {minibatch:0, pos:0.3, neg:0.2},
-      {minibatch:0, pos:0.5, neg:0.3},
-      {minibatch:0, pos:0.6, neg:0.5},
-      {minibatch:0, pos:0.9, neg:0.4},
-      {minibatch:0, pos:0.3, neg:0.2},
-      {minibatch:0, pos:0.4, neg:0.1},
-      {minibatch:0, pos:0.2, neg:0.1}
-  ];
+        const timeline_posneg_data = [
+            {"minibatch": 0, pos: 0.1, neg: -0.0},
+            {"minibatch": 1, pos: 0.3, neg: -0.1},
+            {"minibatch": 2, pos: 0.4, neg: -0.2},
+            {"minibatch": 3, pos: 0.7, neg: -0.1},
+            {"minibatch": 4, pos: 0.2, neg: -0.3},
+            {"minibatch": 5, pos: 0.8, neg: -0.4},
+            {"minibatch": 6, pos: 0.7, neg: -0.5},
+            {"minibatch": 7, pos: 0.4, neg: -0.4},
+            {"minibatch": 8, pos: 0.3, neg: -0.2},
+            {"minibatch": 9, pos: 0.5, neg: -0.3},
+            {"minibatch": 10, pos: 0.6, neg: -0.5},
+            {"minibatch": 11, pos: 0.9, neg: -0.4},
+            {"minibatch": 12, pos: 0.3, neg: -0.2},
+            {"minibatch": 13, pos: 0.4, neg: -0.1},
+            {"minibatch": 14, pos: 0.2, neg: -0.1}
+        ];
 
-    const timeline_pos_data = [
-      {minibatch:0, pos:0.3},
-      {minibatch:0, pos:0.1},
-      {minibatch:0, pos:0.2},
-      {minibatch:0, pos:0.6},
-      {minibatch:0, pos:0.8},
-      {minibatch:0, pos:0.7},
-      {minibatch:0, pos:0.1},
-      {minibatch:0, pos:0.0},
-      {minibatch:0, pos:0.1},
-      {minibatch:0, pos:0.2},
-      {minibatch:0, pos:0.5},
-      {minibatch:0, pos:0.4},
-      {minibatch:0, pos:0.7},
-      {minibatch:0, pos:0.5},
-      {minibatch:0, pos:0.4}
-  ];
+        const timeline_pos_data = [
+            {"minibatch": 0, pos: 0.3},
+            {"minibatch": 1, pos: 0.1},
+            {"minibatch": 2, pos: 0.2},
+            {"minibatch": 3, pos: 0.6},
+            {"minibatch": 4, pos: 0.8},
+            {"minibatch": 5, pos: 0.7},
+            {"minibatch": 6, pos: 0.1},
+            {"minibatch": 7, pos: 0.0},
+            {"minibatch": 8, pos: 0.1},
+            {"minibatch": 9, pos: 0.2},
+            {"minibatch": 10, pos: 0.5},
+            {"minibatch": 11, pos: 0.4},
+            {"minibatch": 12, pos: 0.7},
+            {"minibatch": 13, pos: 0.5},
+            {"minibatch": 14, pos: 0.4}
+        ];
 
   // Create the content using the BaseWidget component.
   const content = (
-    <BaseWidget handleRemove={handleRemove} i={i}>
-      {/* The children contained within the component will be displayed within */}
-      <WidgetHeader>
-          Something
-      </WidgetHeader>
-      <WidgetBody>
-        <div className="TimelineContainer">
-            <ResponsiveBar
-                data={timeline_posneg_data}
-                keys={['pos', 'neg']}
-
-            />
-        </div>
-        <div className="HeatMapContainer">
-            <ResponsiveHeatMap
-            data={heatmap_data}
-            keys={['0', '1', '2', '3', '4', '5', '6', '7']}
-            margin={{ top: 10, right: 10, bottom: 10, left: 10 }}
-            indexBy="row"
-            forceSquare={true}
-            colors="reds"
-            axisLeft={null}
-            axisTop={null}
-            enableLabels={false}
-            />
-        </div>
-      </WidgetBody>
-    </BaseWidget>
+      <BaseWidget handleRemove={handleRemove} i={i}>
+          <WidgetHeader>
+                    Something
+          </WidgetHeader>
+          <WidgetBody>
+              <div className="Left">
+                  <div className="">
+                      Network Training
+                  </div>
+                  <div className="TimelineContainer">
+                      <ResponsiveBar
+                          data={timeline_posneg_data}
+                          keys={['pos', 'neg']}
+                          indexBy="minibatch"
+                          groupMode={'grouped'}
+                          margin={{top: 10, right: 10, bottom: 30, left: 30}}
+                          enableLabel={false}
+                        />
+                  </div>
+              </div>
+              <div className="Right">
+                  <div className="">
+                      Decision Relevance Heat Map
+                  </div>
+                  <div className="HeatMapContainer">
+                      <ResponsiveHeatMap
+                          data={heatmap_data}
+                          keys={['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15']}
+                          margin={{top: 10, right: 10, bottom: 10, left: 10}}
+                          indexBy="row"
+                          forceSquare={true}
+                          colors="reds"
+                          axisLeft={null}
+                          axisTop={null}
+                          enableLabels={false}
+                      />
+                  </div>
+              </div>
+          </WidgetBody>;
+      </BaseWidget>
   );
 
   // The default dimensions in terms of the grid system
