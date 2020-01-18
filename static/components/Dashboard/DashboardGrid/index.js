5c8d05410e5bb16903064bd9ea687154a54162bb
Caramel <octavio.delser@gmail.com>
2020-01-18 19:02:57 +0000

    added_pinch_zoom_for_mobile

--- static/components/Dashboard/DashboardGrid/index.js
+++ static/components/Dashboard/DashboardGrid/index.js
@@ -15,34 +15,36 @@ export default class DashboardGrid extends React.Component {
         console.log("here");
         return (
             <div className={'DashboardGrid'}>
-                <ResponsiveGridLayout
-                    rowHeight={100}
-                    breakpoints={{res5000:10000}}
-                    cols={{res5000:10000/100}}
-                    compactType={null}
-                    draggableCancel={'.nonDraggable'}
-                    autoSize={true}
-                    onWidthChange={() => {
-                        return null
-                    }}
-                >
-                    {this.props.componentDicts.map((componentDict, index) => {
-                        return <div className={'widget'}
-                                    itemevation={3}
-                                    key={index}
-                                    data-grid={{
-                                        i: componentDict.id,
-                                        x: componentDict.x,
-                                        y: componentDict.y,
-                                        h: componentDict.h,
-                                        w: componentDict.w
-                                    }}>
-                            <BaseWidget componentDict={componentDict}
-                                        handleRemove={this.props.handleRemove}
-                                        handleCreate={this.props.handleCreate}/>
-                        </div>
-                    })}
-                </ResponsiveGridLayout>
+
+                    <ResponsiveGridLayout
+                        rowHeight={100}
+                        breakpoints={{res5000: 10000}}
+                        cols={{res5000: 10000 / 100}}
+                        compactType={null}
+                        draggableCancel={'.nonDraggable'}
+                        autoSize={true}
+                        onWidthChange={() => {
+                            return null
+                        }}
+                    >
+                        {this.props.componentDicts.map((componentDict, index) => {
+                            return <div className={'widget'}
+                                        itemevation={3}
+                                        key={index}
+                                        data-grid={{
+                                            i: componentDict.id,
+                                            x: componentDict.x,
+                                            y: componentDict.y,
+                                            h: componentDict.h,
+                                            w: componentDict.w
+                                        }}>
+
+                                <BaseWidget componentDict={componentDict}
+                                            handleRemove={this.props.handleRemove}
+                                            handleCreate={this.props.handleCreate}/>
+                            </div>
+                        })}
+                    </ResponsiveGridLayout>
             </div>
         );
     }
