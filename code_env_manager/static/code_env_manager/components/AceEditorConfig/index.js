f7062e2cc33277f3f5b36d4f3de66041636bd139
Caramel <octavio.delser@gmail.com>
2020-11-23 14:37:12 +0000

    code editor works :D

--- code_env_manager/static/code_env_manager/components/AceEditorConfig/index.js
+++ code_env_manager/static/code_env_manager/components/AceEditorConfig/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ListSelect from "../../../../../static/remote_components/react_components/components/ListSelect";
 import Input from "../../../../../static/remote_components/react_components/components/Input";
+
 /**
  *  Manages an instance of code for editing only
  *  i.e. one file open in provided area
@@ -35,95 +36,87 @@ export default class AceEditorConfig extends React.Component {
 
     render() {
         return (
-            <div className={'AceEditorConfig'}>
-                <ListSelect
-                    object_list={this.mode}
-                    str_key={'name'}
-                    id_key={'name'}
-                    sort_key={'name'}
-                    default={this.mode[0]}
-                    handleSelect={(val) => this.props.onChange({mode: val.name})}
-                />
-                <ListSelect
-                    object_list={this.theme}
-                    str_key={'name'}
-                    id_key={'name'}
-                    sort_key={'name'}
-                    default={this.theme[0]}
-                    handleSelect={(val) => this.props.onChange({theme: val.name})}
-                />
-                <Input
-                    value={this.props.config.name}
-                    name={'name'}
-                    defaultValue={'my code editor'}
-                    onChange={(val) => this.props.onChange({'name': val})}
-                />
-                <Input
-                    value={this.props.config.fontSize}
-                    name={'font size'}
-                    defaultValue={14}
-                    onChange={(val) => this.props.onChange({'fontSize': val})}
-                />
-                <Input
-                    value={this.props.config.showPrintMargin}
-                    name={'show margin'}
-                    type={'checkbox'}
-                    defaultValue={true}
-                    onChange={(val) => this.props.onChange({'showPrintMargin': val})}
-                />
-                <Input
-                    value={this.props.config.showGutter}
-                    name={'showGutter'}
-                    defaultValue={true}
-                    onChange={(val) => this.props.onChange({'showGutter': val})}
-                    type={'checkbox'}
+                <div className={'AceEditorConfig'}>
+                    <ListSelect
+                            object_list={this.mode}
+                            str_key={'name'}
+                            id_key={'name'}
+                            sort_key={'name'}
+                            default={{'name': this.props.config.mode}}
+                            handleSelect={(val) => this.props.onChange({mode: val.name})}
+                    />
+                    <ListSelect
+                            object_list={this.theme}
+                            str_key={'name'}
+                            id_key={'name'}
+                            sort_key={'name'}
+                            default={{'name': this.props.config.theme}}
+                            handleSelect={(val) => this.props.onChange({theme: val.name})}
+                    />
+                    <Input
+                            value={this.props.config.name}
+                            name={'name'}
+                            onChange={(val) => this.props.onChange({'name': val})}
+                    />
+                    <Input
+                            value={this.props.config.fontSize}
+                            name={'font size'}
+                            type={'number'}
+                            onChange={(val) => this.props.onChange({'fontSize': parseInt(val)})}
+                    />
+                    <Input
+                            value={this.props.config.showPrintMargin}
+                            name={'show margin'}
+                            type={'checkbox'}
+                            onChange={(val) => this.props.onChange({'showPrintMargin': val})}
+                    />
+                    <Input
+                            value={this.props.config.showGutter}
+                            name={'showGutter'}
+                            onChange={(val) => this.props.onChange({'showGutter': val})}
+                            type={'checkbox'}
 
-                />
-                <Input
-                    value={this.props.config.highlightActiveLine}
-                    name={'highlightActiveLine'}
-                    defaultValue={true}
-                    type={'checkbox'}
-                    onChange={(val) => this.props.onChange({'highlightActiveLine': val})}
-                />
+                    />
+                    <Input
+                            value={this.props.config.highlightActiveLine}
+                            name={'highlightActiveLine'}
+                            type={'checkbox'}
+                            onChange={(val) => this.props.onChange({'highlightActiveLine': val})}
+                    />
 
-                <Input
-                    value={this.props.config.tabSize}
-                    name={'tabSize'}
-                    defaultValue={2}
-                    onChange={(val) => this.props.onChange({'setOptions': {...this.props.config.setOptions, ...{tabSize: val}}})}
+                    <Input
+                            value={this.props.config.tabSize}
+                            name={'tabSize'}
+                            type={'number'}
+                            onChange={(val) => this.props.onChange({'setOptions': {...this.props.config.setOptions, ...{tabSize: parseInt(val)}}})}
 
-                />
-                <Input
-                    value={this.props.config.enableBasicAutocompletion}
-                    name={'enableBasicAutocompletion'}
-                    type={'checkbox'}
-                    defaultValue={true}
-                    onChange={(val) => this.props.onChange({'setOptions': {...this.props.config.setOptions, ...{enableBasicAutocompletion: val}}})}
-                />
-                <Input
-                    value={this.props.config.enableSnippets}
-                    name={'enableSnippets'}
-                    type={'checkbox'}
-                    defaultValue={false}
-                    onChange={(val) => this.props.onChange({'setOptions': {...this.props.config.setOptions, ...{enableSnippets: val}}})}
-                />
-                <Input
-                    value={this.props.config.showLineNumbers}
-                    name={'showLineNumbers'}
-                    type={'checkbox'}
-                    defaultValue={true}
-                    onChange={(val) => this.props.onChange({'setOptions': {...this.props.config.setOptions, ...{showLineNumbers: val}}})}
-                />
-                <Input
-                    value={this.props.config.enableLiveAutocompletion}
-                    name={'enableLiveAutocompletion'}
-                    type={'checkbox'}
-                    defaultValue={false}
-                    onChange={(val) => this.props.onChange({'setOptions': {...this.props.config.setOptions, ...{enableLiveAutocompletion: val}}})}
-                />
+                    />
+                    <Input
+                            value={this.props.config.enableBasicAutocompletion}
+                            name={'enableBasicAutocompletion'}
+                            type={'checkbox'}
+                            onChange={(val) => this.props.onChange({'setOptions': {...this.props.config.setOptions, ...{enableBasicAutocompletion: val}}})}
+                    />
+                    <Input
+                            value={this.props.config.enableSnippets}
+                            name={'enableSnippets'}
+                            type={'checkbox'}
+                            onChange={(val) => this.props.onChange({'setOptions': {...this.props.config.setOptions, ...{enableSnippets: val}}})}
+                    />
+                    <Input
+                            value={this.props.config.showLineNumbers}
+                            name={'showLineNumbers'}
+                            type={'checkbox'}
+                            onChange={(val) => this.props.onChange({'setOptions': {...this.props.config.setOptions, ...{showLineNumbers: val}}})}
+                    />
+                    <Input
+                            value={this.props.config.enableLiveAutocompletion}
+                            name={'enableLiveAutocompletion'}
+                            type={'checkbox'}
+                            onChange={(val) => this.props.onChange({'setOptions': {...this.props.config.setOptions, ...{enableLiveAutocompletion: val}}})}
+                    />
 
-            </div>
+                </div>
         )
     }
 }
--- code_env_manager/static/code_env_manager/components/AceEditorInstance/index.js
+++ code_env_manager/static/code_env_manager/components/AceEditorInstance/index.js
@@ -4,6 +4,18 @@ import 'ace-builds/webpack-resolver.js'
 import 'ace-builds/src-noconflict/mode-c_cpp'
 import 'ace-builds/src-noconflict/theme-github'
 import 'ace-builds/src-noconflict/theme-monokai'
+import 'ace-builds/src-noconflict/theme-ambiance'
+import 'ace-builds/src-noconflict/theme-dracula'
+import 'ace-builds/src-noconflict/theme-tomorrow'
+import 'ace-builds/src-noconflict/theme-kuroir'
+import 'ace-builds/src-noconflict/theme-twilight'
+import 'ace-builds/src-noconflict/theme-xcode'
+import 'ace-builds/src-noconflict/theme-textmate'
+import 'ace-builds/src-noconflict/theme-solarized_dark'
+import 'ace-builds/src-noconflict/theme-solarized_light'
+import 'ace-builds/src-noconflict/theme-terminal'
+
+
 /**
  *  Manages an instance of code for editing only
  *  i.e. one file open in provided area
@@ -19,27 +31,25 @@ export default class AceEditorInstance extends React.Component {
         console.log("ACE EDITOR: ")
         console.log(this.props.config)
         return (
-            <AceEditor
-                placeholder="Placeholder Text"
-                mode="javascript"
-                theme="monokai"
-                name="blah2"
-                onLoad={this.onLoad}
-                onChange={this.onChange}
-                fontSize={14}
-                showPrintMargin={true}
-                showGutter={true}
-                highlightActiveLine={true}
-                value={`function onLoad(editor) {
-  console.log("i've loaded");
-}`}
-                setOptions={{
-                    enableBasicAutocompletion: false,
-                    enableLiveAutocompletion: false,
-                    enableSnippets: false,
-                    showLineNumbers: true,
-                    tabSize: 2,
-                }}/>
+                <AceEditor
+                        placeholder={this.props.config.placeholder}
+                        mode={this.props.config.mode}
+                        theme={this.props.config.theme}
+                        name={this.props.config.name}
+                        onLoad={this.props.config.onLoad}
+                        onChange={this.props.config.onChange}
+                        fontSize={this.props.config.fontSize}
+                        showPrintMargin={this.props.config.showPrintMargin}
+                        showGutter={this.props.config.showGutter}
+                        highlightActiveLine={this.props.config.highlightActiveLine}
+                        value={this.props.config.value}
+                        setOptions={{
+                            enableBasicAutocompletion: this.props.config.setOptions.enableBasicAutocompletion,
+                            enableLiveAutocompletion: this.props.config.setOptions.enableLiveAutocompletion,
+                            enableSnippets: this.props.config.setOptions.enableSnippets,
+                            showLineNumbers: this.props.config.setOptions.showLineNumbers,
+                            tabSize: this.props.config.setOptions.tabSize,
+                        }}/>
 
         )
     }
